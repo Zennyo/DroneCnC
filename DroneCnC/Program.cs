3ad2ff462b979283a80bb85b4d9dae2074712fdc
Zennyo <zennygami@gmail.com>
2020-09-02 14:23:41 +0200

    service and chara

--- DroneCnC/Program.cs
+++ DroneCnC/Program.cs
@@ -5,6 +5,16 @@ using Windows.Devices.Bluetooth;
 using Windows.Devices.Scanners;
 using Windows.Devices.Bluetooth.Advertisement;
 using Windows.Devices.Enumeration;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+using System.Collections.Generic;
+
+
+/*
+ * Service: 00008650-0000-1000-8000-00805f9b34fb
+ * --- Characteristic: 00008651-0000-1000-8000-00805f9b34fb <---
+ * --- Characteristic: 00008655-0000-1000-8000-00805f9b34fb
+ * --- Characteristic: 0000865f-0000-1000-8000-00805f9b34fb
+ */
 
 namespace DroneCnC
 {
@@ -12,6 +22,9 @@ namespace DroneCnC
     {
         BluetoothLEDevice mSteering;
         const string mSteeringAdd = "C686A104C90D";
+        const string mService = "00008650-0000-1000-8000-00805f9b34fb";
+        const string mCharacteristic = "00008651-0000-1000-8000-00805f9b34fb";
+        GattCharacteristic mRegisteredCharacteristic;
         bool mFound = false;
 
         static void Main(string[] args)
@@ -53,15 +66,44 @@ namespace DroneCnC
         {
             Console.WriteLine("Connecting...");
             mSteering = await BluetoothLEDevice.FromBluetoothAddressAsync(deviceAdd);
-            if(mSteering == null)
+            if (mSteering == null)
             {
                 Console.WriteLine("failed to connect");
             }
             else
             {
-                var result = await mSteering.DeviceInformation.Pairing.PairAsync();
-                Console.WriteLine("status: " + result.Status);
+                var ServResult = await mSteering.GetGattServicesAsync(BluetoothCacheMode.Uncached);
+                Console.WriteLine("status: " + ServResult.Status);
+                if (ServResult.Status == GattCommunicationStatus.Success)
+                {
+                    IReadOnlyList<GattCharacteristic> characteristics = null;
+
+                    var services = ServResult.Services;
+                    foreach (var service in services)
+                    {
+                        var CharResult = await service.GetCharacteristicsAsync(BluetoothCacheMode.Uncached);
+                        Console.WriteLine("Service: " + service.Uuid.ToString());
+                        if (CharResult.Status == GattCommunicationStatus.Success)
+                        {
+                            characteristics = CharResult.Characteristics;
+                            foreach(GattCharacteristic chara in characteristics)
+                            {
+                                Console.WriteLine(" --- Characteristic: " + chara.Uuid);
+                                if(chara.Uuid.ToString().Equals(mCharacteristic))
+                                {
+                                    Console.WriteLine(" --- --- chara found!trying to subscribe to " + chara.Uuid.ToString());
+                                    mRegisteredCharacteristic = chara;
+                                    mRegisteredCharacteristic.ValueChanged += Characteristic_ValueChanged;
+                                }
+                            }
+                        }
+                    }
+                }
             }
+        }
+        private void Characteristic_ValueChanged(GattCharacteristic sender, GattValueChangedEventArgs args)
+        {
+            Console.WriteLine("CHANGED!");
         }
     }
 }
